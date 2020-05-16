1b33f48e71313ff7d5a3692bd1f65d5c844768b6
dheatovwil <tgtay@outlook.com>
2020-05-16 13:28:23 +0800

    Add support for rendering series
    Can visualize the graph state at each step

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
 import gen
+import os
+
+
+class Series:
+    def __init__(self):
+        try:
+            os.mkdir("out")
+        except FileExistsError:
+            os.system("rm -rf out")
+        self.ctr = 0
+
+    def add(self, root):
+        gen.gen_dot(root).render(str(self.ctr).zfill(2), directory="out", cleanup=True, format="png")
+        self.ctr += 1
 
 
 def visualize(root):
